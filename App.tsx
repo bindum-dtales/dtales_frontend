f1abe2901c3e1656e9017d49d07483d77025f502
siddhanth36 <sksiddhanth69@gmail.com>
2026-02-26 12:46:35 +0530

    v63

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
-import { motion, AnimatePresence } from 'framer-motion';
 import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -38,24 +37,14 @@ const ProtectedRoute: React.FC<{ children: React.ReactElement }> = ({ children }
 };
 
 const App: React.FC = () => {
-  const location = useLocation();
-
   return (
     <>
       <ScrollToTop />
       <div className="flex flex-col min-h-screen bg-[#EEEEEE] font-sans text-black">
         <Navbar />
         <div className="flex-grow">
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={location.pathname}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              transition={{ duration: 0.4, ease: 'easeInOut' }}
-            >
-              <Routes>
-                <Route path="/" element={<Home />} />
+          <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/team" element={<Team />} />
             <Route path="/services" element={<Services />} />
             <Route path="/portfolio" element={<Portfolio />} />
@@ -138,9 +127,7 @@ const App: React.FC = () => {
               } 
             />
             <Route path="*" element={<Navigate to="/" replace />} />
-              </Routes>
-            </motion.div>
-          </AnimatePresence>
+          </Routes>
         </div>
         <Footer />
       </div>
